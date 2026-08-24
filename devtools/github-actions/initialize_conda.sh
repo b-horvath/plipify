bd6a315f7c199df7750996aa6e943c21e80a88b2
Ben <beho00003@uni-saarland.de>
2026-08-24 11:18:42 +0200

    initial fix for CI on test (ubuntu-latest, 3.7)

--- devtools/github-actions/initialize_conda.sh
+++ devtools/github-actions/initialize_conda.sh
@@ -2,6 +2,10 @@
 
 # $CI_OS is $matrix.os, as exported in GHA *.yaml
 # $CONDA (miniconda installation path) is always defined in the GHA virtual environments
+
+# Ensure that YAML parser is present before we run python scripts
+python -m pip install --quiet pyyaml
+
 case ${CI_OS} in
     windows*)
         eval "$(${CONDA}/condabin/conda.bat shell.bash hook)";;
